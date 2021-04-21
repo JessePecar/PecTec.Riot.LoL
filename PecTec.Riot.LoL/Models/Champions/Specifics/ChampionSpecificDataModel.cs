1b76d33c7b3785f6168cae95e5c5a624c6919023
Jesse Pecar <Jesse.Pecar@publix.com>
2021-04-21 16:58:49 -0400

    accidently had the root object inside an object

--- PecTec.Riot.LoL/Models/Champions/Specifics/ChampionSpecificDataModel.cs
+++ PecTec.Riot.LoL/Models/Champions/Specifics/ChampionSpecificDataModel.cs
@@ -6,12 +6,10 @@ namespace PecTec.Riot.LoL.Models.Champions.Specifics
 {
     public class ChampionSpecificDataModel
     {
-        public class Root
-        {
-            public string Type { get; set; }
-            public string Format { get; set; }
-            public string Version { get; set; }
-            public Dictionary<string, ChampionSpecificModel> Data { get; set; }
-        }
+        public string Type { get; set; }
+        public string Format { get; set; }
+        public string Version { get; set; }
+        public Dictionary<string, ChampionSpecificModel> Data { get; set; }
+
     }
 }
